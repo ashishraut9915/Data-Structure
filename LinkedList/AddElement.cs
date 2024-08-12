171cac90d04c712c7c8012e8c6b2c537cabe472a
[Ashish Raut] <ashish.raut9915@gmail.com>
2024-08-12 23:24:13 +0530

    [Smita]Add Element

---
+++ LinkedList/AddElement.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+class AddElements
+{
+    static void Main()
+    {
+        
+        LinkedList<string> linkedList = new LinkedList<string>();
+
+        linkedList.AddLast("Apple");
+        linkedList.AddLast("Banana");
+        linkedList.AddLast("Cherry");
+
+        Console.WriteLine("Elements in the LinkedList:");
+        foreach (var item in linkedList)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
