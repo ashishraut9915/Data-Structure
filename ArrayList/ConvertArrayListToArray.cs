032ec2b664d59253ae882debb0b01c9f49165069
[Ashish Raut] <ashish.raut9915@gmail.com>
2024-08-12 22:29:54 +0530

    [smita]ArrayList

---
+++ ArrayList/ConvertArrayListToArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayList_Programs
+{
+    internal class ConvertArrayListToArray
+    {
+        public static void Main(String[] args)
+        {
+            ArrayList list = new ArrayList() { "One", "Two", "Three", "Four" };
+
+
+            object[] array = list.ToArray();
+
+
+            Console.WriteLine("Array Elements:");
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }      
+}
+
